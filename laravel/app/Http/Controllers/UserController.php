d9c379bea7350f3db086a8a40e0d017c7718b957
Nikivas <nikivas97@mail.ru>
2019-08-31 01:18:51 +0300

    added sorting and revorked chats/get

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Chat;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
+
 class UserController extends Controller
 {
     public function add(Request $request){
@@ -15,11 +17,18 @@ class UserController extends Controller
 
     public function getChats(Request $request){
         $json = json_decode($request->getContent());
-        $chats = \App\Models\Chat::with(['userInfo'=> function($q) use($json){
-            $q->where('user_id','=',$json->user);
-        }])->get();
-        return json_encode($chats);
 
+        $messages = DB::table('messages')
+            ->select('chat_id', DB::raw('MAX(created_at) as last_post_time') )
+            ->groupBy('chat_id');
+
+        $chats = DB::table('chats_users')
+            ->join('chats','chats_users.chat_id', '=', 'chats.id')
+            ->joinSub($messages,'last_messages',function($join){
+                $join->on('chats.id','=','last_messages.chat_id');
+            })->orderByDesc('last_messages.last_post_time')->select('chats.id', 'chats.name', 'chats.created_at', 'last_messages.last_post_time')->distinct()->get();
+
+        return json_encode($chats);
     }
     
 }
