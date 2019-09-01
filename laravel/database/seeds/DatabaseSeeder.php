3d125b5214716c921e2f0d09b35a2c192159af38
Nikivas <nikivas97@mail.ru>
2019-09-01 15:20:39 +0300

    seeds added

--- laravel/database/seeds/DatabaseSeeder.php
+++ laravel/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use \App\Models\User;
+use \App\Models\Chat;
+use \App\Models\Message;
+use \App\Models\ChatUsers;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,7 +15,39 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $user = \App\Models\User::create(['username' => 'nikivas']);
+        $user1 = User::create(['username' => 'user1']);
+        $user2 = User::create(['username' => 'user2']);
+
+        $chat1 = Chat::create(['name'=> 'chat1']);
+        $chat2 = Chat::create(['name'=> 'chat2']);
+
+        ChatUsers::create(['user_id'=>$user1->id, 'chat_id' => $chat1->id]);
+        ChatUsers::create(['user_id'=>$user2->id, 'chat_id' => $chat1->id]);
+
+        ChatUsers::create(['user_id'=>$user1->id, 'chat_id' => $chat2->id]);
+
+        Message::create([
+            'user_id' => $user1->id, 
+            'chat_id' => $chat1->id, 
+            'text' => 'text'
+        ]);
+        Message::create([
+            'user_id' => $user2->id, 
+            'chat_id' => $chat1->id,
+            'text' => 'text'
+        ]);
+        Message::create([
+            'user_id' => $user1->id, 
+            'chat_id' => $chat2->id, 
+            'text' => 'text'
+        ]);
+        Message::create([
+            'user_id' => $user1->id, 
+            'chat_id' => $chat1->id, 
+            'text' => 'text'
+        ]);
+
+        
         
     }
 }
