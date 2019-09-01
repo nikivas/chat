7b10064d3f4911099a89f5be1ab1188d7f22bfb7
Nikivas <nikivas97@mail.ru>
2019-09-01 15:31:16 +0300

    message relation on author and chat

--- laravel/app/Models/Chat.php
+++ laravel/app/Models/Chat.php
@@ -7,9 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Chat extends Model
 {
     protected $fillable = ['name'];
+
     public function users(){
         return $this->belongsToMany('App\Models\User','chats_users', 'chat_id', 'user_id');
     }
+    
     public function userInfo(){
         return $this->hasMany('\App\Models\ChatUsers','chat_id','id');
     }
--- laravel/app/Models/Message.php
+++ laravel/app/Models/Message.php
@@ -8,4 +8,11 @@ class Message extends Model
 {
     protected $fillable = ['user_id', 'chat_id', 'text'];
     
+    public function chat(){
+        return $this->hasOne('App\Models\Chat','id','chat_id');
+    }
+
+    public function author(){
+        return $this->hasOne('App\Models\User','id','user_id');
+    }
 }
